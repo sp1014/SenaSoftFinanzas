03482715f8dce216fc691071bd214f5054142021
Santiago Perez <80767619+sp1014@users.noreply.github.com>
2021-10-21 16:32:09 -0500

    Update ClienteInternoController.php

--- APP/Controllers/ClienteInternoController.php
+++ APP/Controllers/ClienteInternoController.php
@@ -1,16 +1,15 @@
 <?php
+//Se incluye el modelo donde conectará el controlador.
+
 require_once('App/Models/ClienteInterno.php');
-/**
- * Controla el inicio de sesión.
- */
-class ClienteController
-{
+
+class UserController{
+
     private $model;
-    /**
-     * Método constructor que carga el archivo del modelo que va a estar asociado. 
-     */
+
+ 
     public function __CONSTRUCT()
-    { $this->model = new cliente();
+    { $this->model = new admin();
         
     }
     /**
@@ -19,80 +18,37 @@ class ClienteController
     public function index()
     {
         $data['tittle'] = 'Usuarios | Finanzal Fast';
-        require_once('Public/Views/clienteinterno/clienteinterno.php');
+        require_once 'view/clienteexterno/clienteexterno.php';
     }
 
 
-    public function Crud(){
-        $pvd = new cliente();
 
-        //Se obtienen los datos de los usuarios a editar.
+    //Llamado a la vista proveedor-editar
+    public function Crud(){
+        $pvd = new user();
+
+        //Se obtienen los datos del proveedor a editar.
         if(isset($_REQUEST['id'])){
             $pvd = $this->model->Obtener($_REQUEST['id']);
         }
 
         //Llamado de las vistas.
-      
-        require_once 'Public/Views/admin/admin-editar.php';
-      
+        require_once 'view/header.php';
+        require_once 'view/user/user-editar.php';
+        require_once 'view/footer.php';
   }
 
-    //Llamado a la vista admin-nuevo
-    public function Nuevo(){
-        $pvd = new cliente();
+    //Llamado a la vista proveedor-nuevo
 
-        //Llamado de las vistas.
-      
-        require_once 'Public/Views/cliente/clienteinterno.php';
-      
-    }
 
-    //Método que registrar al modelo un nuevo usuario.
-    public function Guardar(){
-        $pvd = new cliente();
+    //Método que registrar al modelo un nuevo proveedor.
 
-        //Captura de los datos del formulario (vista).
-        $pvd->id = $_REQUEST['id'];
-        $pvd->nombre = $_REQUEST['nombre'];
-        $pvd->id_tipodocumento = $_REQUEST['id_tipodocumento'];
-        $pvd->numero_documento = $_REQUEST['numero_documento'];
-        $pvd->telefono = $_REQUEST['telefono'];
-        $pvd->correo = $_REQUEST['correo'];
-        $pvd->pass = $_REQUEST['pass'];
-        $pvd->tipo_rol = $_REQUEST['tipo_rol'];
-        $pvd->estado = $_REQUEST['estado'];
-        //Registro al modelo Usuarios.
-        $this->model->Registrar($pvd);
 
-        header('Location: ?c=cliente&a=Nuevo.php');
-    }
+ 
 
-    //Método que modifica el modelo de un Usuario.
-    public function Editar(){
-        $pvd = new cliente();
-
-        $pvd->id = $_REQUEST['id'];
-        $pvd->nombre = $_REQUEST['nombre'];
-        $pvd->id_tipodocumento = $_REQUEST['id_tipodocumento'];
-        $pvd->numero_documento = $_REQUEST['numero_documento'];
-        $pvd->telefono = $_REQUEST['telefono'];
-        $pvd->correo = $_REQUEST['correo'];
-        $pvd->pass = $_REQUEST['pass'];
-        $pvd->tipo_rol = $_REQUEST['tipo_rol'];
-        $pvd->estado = $_REQUEST['estado'];
-
-        $this->model->Actualizar($pvd);
-
-        header('Location: ?c=cliente&a=insert');
-    }
-
-    //Método que elimina el Usuario con el id dado.
-    public function Eliminar(){
-        $this->model->Eliminar($_REQUEST['id']);
-        header('Location: ?c=cliente&a=Nuevo.php');
-    }
+    //Método que elimina la tupla proveedor con el nit dado.
+   
 }
-
 
 
 
