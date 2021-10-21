f6667789fce59fe969ed855c42f5da3c2d6d43dd
Sergio <svztecnologia@gmail.com>
2021-10-21 10:29:14 -0500

    make user datatable

--- Public/Views/admin/admin.php
+++ Public/Views/admin/admin.php
@@ -8,11 +8,20 @@
     <title><?= $data['tittle'] ?></title>
     <meta content="Responsive admin theme build on top of Bootstrap 4" name="description" />
     <meta content="Themesdesign" name="author" />
+    
     <link rel="shortcut icon" href="Public/Assets/images/icono.png">
     <link href="Public/Assets/css/bootstrap.min.css" rel="stylesheet" type="text/css">
     <link href="Public/Assets/css/metismenu.min.css" rel="stylesheet" type="text/css">
     <link href="Public/Assets/css/icons.css" rel="stylesheet" type="text/css">
     <link href="Public/Assets/css/style.css" rel="stylesheet" type="text/css">
+
+     <!-- DataTables -->
+     <link href="Public/Assets/plugins/datatables/dataTables.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+    <link href="Public/Assets/plugins/datatables/buttons.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+
+    <!-- Responsive datatable examples -->
+    <link href="Public/Assets/plugins/datatables/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
+
 </head>
 
 <body>
@@ -20,67 +29,126 @@
     require_once('Public/Views/vector/header.php');
     ?>
 
-<h1 class="page-header">Usuario</h1>
+    <div class="content-page">
+                <!-- Start content -->
+                <div class="content">
+                    <div class="container-fluid">
+                        <div class="page-title-box">
+                            <div class="row align-items-center">
+                                <div class="col-sm-6">
+                                    <h4 class="page-title">Usuario</h4>
+                                </div>
+                                <div class="col-sm-6">
+                                    <ol class="breadcrumb float-right">
+                                        <li class="breadcrumb-item"><a href="javascript:void(0);">Inicio</a></li>
+                                        <li class="breadcrumb-item"><a href="javascript:void(0);">Usuario</a></li>
+                                    </ol>
+                                </div>
+                            </div> <!-- end row -->
+                        </div>
+                        <!-- end page-title -->
+                        <div class="well well-sm text-right">
+                        <a class="btn btn-primary" href="?c=admin&a=Nuevo">Nuevo Usuario</a>
+                        </div>
+                        <span>-</span>
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card m-b-30">
+                                    <div class="card-body">
+        
+                                        <h4 class="mt-0 header-title">Usuarios registrados</h4>
+                                        <p class="sub-title"></code>
+                                        </p>
+        
+                                        <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                                            <thead>
+                                                <tr>
+                                                    <th>Id</th>
+                                                    <th>Nombre</th>
+                                                    <th>Tipo Documento</th>
+                                                    <th>Numero Documento</th>
+                                                    <th>Telefono</th>
+                                                    <th>Correo</th>
+                                                    <th>Clave</th>
+                                                    <th>Tipo de Rol</th>
+                                                    <th>Estado</th>
+                                                    <th>Acciones</th>
+                                                    <th>Acciones</th>
+                                                </tr>
+                                            </thead>
+        
+                                            
+                                        <tbody>
+                                                                            
+                                        <?php foreach($this->model->Listar() as $r): ?>
+                                            <tr>
+                                                <td><?php echo $r->id; ?></td>
+                                                <td><?php echo $r->nombre; ?></td>
+                                                <td><?php echo $r->id_tipodocumento; ?></td>
+                                                <td><?php echo $r->numero_documento; ?></td>
+                                                <td><?php echo $r->telefono; ?></td>
+                                                <td><?php echo $r->correo; ?></td>
+                                                <td><?php echo $r->pass; ?></td>
+                                                <td><?php echo $r->tipo_rol; ?></td>
+                                                <td><?php echo $r->estado; ?></td>
+                                                <td>
+                                             
+                                                    <a href="?c=admin&a=Crud&id=<?php echo $r->id; ?>" class="btn btn-primary">Editar</a>
+                                                </td>
+                                                <td>
+                                               
+                                                <a onclick="javascript:return confirm('¿Seguro de eliminar este registro?');" href="?c=admin&a=Eliminar&id=<?php echo $r->id;?>" class="btn btn-primary">Eliminar</a>
+                                                </td>
+                                                
+                                            </tr>
+                                            
+                                        <?php endforeach; ?>
+                                        
+                                        </tbody>
+                                        </table>
+        
+                                    </div>
+                                </div>
+                            </div> <!-- end col -->
+                        </div> <!-- end row -->
 
-<div class="well well-sm text-right">
-    <a class="btn btn-primary" href="?c=admin&a=Nuevo">Nuevo Usuario</a>
-    
-</div>
-
-<table class="table table-striped">
-    <thead>
-        <tr>
-            <th style="width:180px;">Id</th>
-            <th style="width:120px;">Nombre</th>
-            <th style="width:120px;">Tipo Documento</th>
-            <th style="width:120px;">Numero Documento</th>
-            <th style="width:120px;">Telefono</th>
-            <th style="width:120px;">Correo</th>
-            <th style="width:120px;">Clave</th>
-            <th style="width:120px;">Tipo de Rol</th>
-            <th style="width:120px;">Estado</th>
-        </tr>
-    </thead>
-    <tbody>
-    <?php foreach($this->model->Listar() as $r): ?>
-        <tr>
-            <td><?php echo $r->id; ?></td>
-            <td><?php echo $r->nombre; ?></td>
-            <td><?php echo $r->id_tipodocumento; ?></td>
-            <td><?php echo $r->numero_documento; ?></td>
-            <td><?php echo $r->telefono; ?></td>
-            <td><?php echo $r->correo; ?></td>
-            <td><?php echo $r->pass; ?></td>
-            <td><?php echo $r->tipo_rol; ?></td>
-            <td><?php echo $r->estado; ?></td>
-            <td>
-                <a href="?c=admin&a=Crud&id=<?php echo $r->id; ?>">Editar</a>
-            </td>
-            <td>
-                <a onclick="javascript:return confirm('¿Seguro de eliminar este registro?');" href="?c=admin&a=Eliminar&id=<?php echo $r->id; ?>">Eliminar</a>
-            </td>
-        </tr>
-    <?php endforeach; ?>
-    </tbody>
-</table>
+                    
 
     <!-- END wrapper -->
     <?php
     require_once('Public/Views/vector/footer.php');
     require_once('Public/Views/components/scripts.php');
     ?>
-    <!-- jQuery  -->
-    <script src="Public/Assets/js/jquery.min.js"></script>
-    <script src="Public/Assets/js/bootstrap.bundle.min.js"></script>
-    <script src="Public/Assets/js/metismenu.min.js"></script>
-    <script src="Public/Assets/js/jquery.slimscroll.js"></script>
-    <script src="Public/Assets/js/waves.min.js"></script>
+    
+     <!-- jQuery  -->
+     <script src="Public/Assets/js/jquery.min.js"></script>
+        <script src="Public/Assets/js/bootstrap.bundle.min.js"></script>
+        <script src="Public/Assets/js/metismenu.min.js"></script>
+        <script src="Public/Assets/js/jquery.slimscroll.js"></script>
+        <script src="Public/Assets/js/waves.min.js"></script>
 
-    <!-- Parsley js -->
-    <script src="Public/Assets/plugins/parsleyjs/parsley.min.js"></script>
+        <!-- Required datatable js -->
+        <script src="Public/Assets/plugins/datatables/jquery.dataTables.min.js"></script>
+        <script src="Public/Assets/plugins/datatables/dataTables.bootstrap4.min.js"></script>
+        <!-- Buttons examples -->
+        <script src="Public/Assets/plugins/datatables/dataTables.buttons.min.js"></script>
+        <script src="Public/Assets/plugins/datatables/buttons.bootstrap4.min.js"></script>
+        <script src="Public/Assets/plugins/datatables/jszip.min.js"></script>
+        <script src="Public/Assets/plugins/datatables/pdfmake.min.js"></script>
+        <script src="Public/Assets/plugins/datatables/vfs_fonts.js"></script>
+        <script src="Public/Assets/plugins/datatables/buttons.html5.min.js"></script>
+        <script src="Public/Assets/plugins/datatables/buttons.print.min.js"></script>
+        <script src="Public/Assets/plugins/datatables/buttons.colVis.min.js"></script>
+        <!-- Responsive examples -->
+        <script src="Public/Assets/plugins/datatables/dataTables.responsive.min.js"></script>
+        <script src="Public/Assets/plugins/datatables/responsive.bootstrap4.min.js"></script>
+
+        <!-- Datatable init js -->
+        <script src="Public/Assets/pages/datatables.init.js"></script>   
+
     <!-- App js -->
-    <script src="Public/Assets/js/app.js" defer></script>
-    <script src="Public/Assets/js/functions/clienteexterno.js" defer></script>
+    <script src="Public/Assets/js/app.js" defer></script>                                     
+
 </body>
 
 </html>
