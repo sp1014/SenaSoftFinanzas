4261d89f2620ffe713beb393f6a85d29f962a857
Santiago Perez <80767619+sp1014@users.noreply.github.com>
2021-10-20 23:43:51 -0500

    model and controler of admin

---
+++ Public/Views/admin/admin-nuevo.php
@@ -0,0 +1,72 @@
+<h1 class="page-header">
+    Nuevo Registro
+</h1>
+
+<ol class="breadcrumb">
+  <li><a href="?c=admin">Usuario</a></li>
+  <li class="active">Nuevo Registro</li>
+</ol>
+
+<form id="frm-admin" action="?c=admin&a=Guardar" method="post" enctype="multipart/form-data">
+
+
+<div class="form-group">
+        <label>id</label>
+        <input type="text" name="id" value="<?php echo $pvd->id; ?>" class="form-control" data-validacion-tipo="requerido|min:100" />
+    </div>
+
+
+    <div class="form-group">
+        <label>Nombre</label>
+        <input type="text" name="nombre" value="<?php echo $pvd->nombre; ?>" class="form-control"  data-validacion-tipo="requerido|min:100" />
+    </div>
+
+    <div class="form-group">
+        <label>Tipo Documento</label>
+        <input type="text" name="id_tipodocumento" value="<?php echo $pvd->id_tipodocumento; ?>" class="form-control"  data-validacion-tipo="requerido|min:100" />
+    </div>
+
+    <div class="form-group">
+        <label>Numero Docuemnto</label>
+        <input type="text" name="numero_documento" value="<?php echo $pvd->numero_documento; ?>" class="form-control"  data-validacion-tipo="requerido|min:10" />
+    </div>
+
+    <div class="form-group">
+        <label>Telefono</label>
+        <input type="text" name="telefono" value="<?php echo $pvd->telefono; ?>" class="form-control"  data-validacion-tipo="requerido|min:10" />
+    </div>
+
+    <div class="form-group">
+        <label>Correo</label>
+        <input type="text" name="correo" value="<?php echo $pvd->correo; ?>" class="form-control"  data-validacion-tipo="requerido|min:10" />
+    </div>
+
+    <div class="form-group">
+        <label>Clave</label>
+        <input type="text" name="pass" value="<?php echo $pvd->pass; ?>" class="form-control" data-validacion-tipo="requerido|min:10" />
+    </div>
+
+    <div class="form-group">
+        <label>Rol</label>
+        <input type="text" name="tipo_rol" value="<?php echo $pvd->tipo_rol; ?>" class="form-control"  data-validacion-tipo="requerido|min:10" />
+    </div>
+
+    <div class="form-group">
+        <label>Estado</label>
+        <input type="text" name="estado" value="<?php echo $pvd->estado; ?>" class="form-control"  data-validacion-tipo="requerido|min:10" />
+    </div>
+
+    <hr />
+
+    <div class="text-right">
+        <button class="btn btn-success">Guardar</button>
+    </div>
+</form>
+
+<script>
+    $(document).ready(function(){
+        $("#frm-admin").submit(function(){
+            return $(this).validate();
+        });
+    })
+</script>
