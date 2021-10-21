95309a6fd684c5b96a073e3c840ea70f6f606587
Santiago Perez <80767619+sp1014@users.noreply.github.com>
2021-10-21 17:12:40 -0500

    Update- title -Admin

--- Public/Views/admin/admin.php
+++ Public/Views/admin/admin.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=0, minimal-ui">
-    <title><?= $data['tittle'] ?></title>
+    <title>Administrador</title>
     <meta content="Responsive admin theme build on top of Bootstrap 4" name="description" />
     <meta content="Themesdesign" name="author" />
 
