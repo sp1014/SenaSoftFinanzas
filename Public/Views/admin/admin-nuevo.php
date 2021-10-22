a56bf38e2f11d86a120069503873104c7c89439f
Sergio <svztecnologia@gmail.com>
2021-10-21 22:35:32 -0500

    make changes to the index and I am changing the layout of the form

--- Public/Views/admin/admin-nuevo.php
+++ Public/Views/admin/admin-nuevo.php
@@ -15,55 +15,52 @@
     <!-- Responsive datatable examples -->
     <link href="Public/Assets/plugins/datatables/responsive.bootstrap4.min.css" rel="stylesheet" type="text/css" />
     <!-- end page-title -->
-    <div class="row  d-flex justify-content-center">
+    <div class="form-row  d-flex justify-content-center">
         <div class="col-lg-5">
             <div class="card m-b-30">
                 <div class="card-body">
 
                     <form id="frm-admin" action="?c=admin&a=Guardar" method="post" enctype="multipart/form-data">
 
-                    <div class="form-group">
+                    <div class="form-group col-md-6">
                    <label> Seleccione la Voz:</label> 
-                   <select  id='voiceList' class="form-control"> 
+                   <select  id='voiceList' class="form-control"> </select>
                     </div>
 
-                        <div class="form-group">
-                            <label> Seleccione la Voz:</label>
-                            <select id='voiceList' class="form-control">
-                        </div>
-                        <div class="form-group">
+               
+                        <div class="form-group col-md-6">
                             <label>Nombre</label>
                             <input type="text" id="nombre" name="nombre" value="<?php echo $pvd->nombre; ?>" class="form-control" required />
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group col-md-6">
                             <label>Tipo Documento</label>
                             <!-- <input type="text" name="id_tipodocumento" value="" class="form-control"  data-validacion-tipo="requerido|min:100" />-->
                             <select name="id_tipodocumento" id="txtTipoDoc" class="form-control" value="<?php echo $pvd->id_tipodocumento; ?>" required>
                             </select>
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group col-md-6">
                             <label>Numero Documento</label>
                             <input type="text" id="numero_documento" name="numero_documento" data-parsley-type="number" value="<?php echo $pvd->numero_documento; ?>" class="form-control" required data-validacion-tipo="requerido|min:10" />
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group col-md-6">
                             <label>Teléfono</label>
                             <input type="text" id="telefono" name="telefono" data-parsley-type="number" value="<?php echo $pvd->telefono; ?>" class="form-control" required data-validacion-tipo="requerido|min:10" />
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group col-md-6">
                             <label>Correo</label>
                             <input type="text" id="correo" name="correo" value="<?php echo $pvd->correo; ?>" data-parsley-type="email" class="form-control" data-validacion-tipo="requerido|min:10" />
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group col-md-6">
                             <label>Clave</label>
                             <input type="password" name="pass" value="<?php echo $pvd->pass; ?>" data-parsley-type="password" class="form-control" required data-validacion-tipo="requerido|min:10" />
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group col-md-6">
                             <label>Rol</label>
                             <select class="form-select form-control" name="tipo_rol" value="<?php echo $pvd->tipo_rol; ?>" required>
                                 <option value="Administrador">Administrador</option>
@@ -72,14 +69,14 @@
                             </select>
                         </div>
 
-                        <div class="form-group">
+                        <div class="form-group col-md-6">
                             <label>Estado</label>
                             <select class="form-select form-control" name="estado" value="<?php echo $pvd->estado; ?>" required>
                                 <option value="1">Activo</option>
                                 <option value="0">Inactivo</option>
                             </select>
                         </div>
-                        <div class="form-group">
+                        <div class="form-group col-md-6">
                             <div>
                                 <button type="submit" class="btn btn-primary waves-effect waves-light">
                                     Enviar
@@ -91,6 +88,9 @@
             </div>
         </div> <!-- end col -->
 
+
+
+        
         <?php
         require_once('Public/Views/vector/footer.php');
         require_once('Public/Views/components/scripts.php');
@@ -106,7 +106,6 @@
         <script src="Public/Assets/plugins/parsleyjs/parsley.min.js"></script>
         <!-- App js -->
         <script src="Public/Assets/js/app.js" defer></script>
-        <script src="Public/Assets/js/functions/clienteexterno.js"></script>
         <script defer>
             $(document).ready(function() {
                 $('form').parsley();
--- Public/Views/dashboard/index.php
+++ Public/Views/dashboard/index.php
@@ -10,85 +10,90 @@
     <link href="Public/Assets/css/icons.css" rel="stylesheet" type="text/css">
     <link href="Public/Assets/css/style.css" rel="stylesheet" type="text/css">
 </head>
-
 <body>
     <?php
     require_once('Public/Views/vector/header.php');
     ?>
+        <h1 class="titulo">Financialfast</h1>
     <div class="row">
-        <div class="col-sm-6 col-xl-3">
-            <div class="card">
-                <div class="card-heading p-4">
-                    <div class="mini-stat-icon float-right">
-                        <i class="mdi mdi-cube-outline bg-primary  text-white"></i>
-                    </div>
-                    <div>
-                        <h5 class="font-20">Sesiones activas</h5>
-                    </div>
-                    <h3 class="mt-4">4</h3>
-                    <div class="progress mt-4" style="height: 4px;">
-                        <div class="progress-bar bg-primary" role="progressbar" style="width: 100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <span class="float-right">100%</span>
-                </div>
+    
+<div class="col-sm-6 col-xl-3">
+    <div class="card">
+        <div class="card-heading p-4">
+            <div class="mini-stat-icon float-right">
+                <i class="mdi mdi-cube-outline bg-primary  text-white"></i>
             </div>
-        </div>
-        <br>
-        <div class="col-sm-6 col-xl-3">
-            <div class="card">
-                <div class="card-heading p-4">
-                    <div class="mini-stat-icon float-right">
-                        <i class="mdi mdi-briefcase-check bg-success text-white"></i>
-                    </div>
-                    <div>
-                        <h5 class="font-20">Categorias actuales</h5>
-                    </div>
-                    <h3 class="mt-4">2</h3>
-                    <div class="progress mt-4" style="height: 4px;">
-                        <div class="progress-bar bg-success" role="progressbar" style="width:100%" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <span class="float-right">100%</span>
-                </div>
+            <div>
+                <h5 class="font-16">Active Sessions</h5>
             </div>
-        </div>
-
-        <div class="col-sm-6 col-xl-3">
-            <div class="card">
-                <div class="card-heading p-4">
-                    <div class="mini-stat-icon float-right">
-                        <i class="mdi mdi-tag-text-outline bg-warning text-white"></i>
-                    </div>
-                    <div>
-                        <h5 class="font-20">Archivos</h5>
-                    </div>
-                    <h3 class="mt-4">1</h3>
-                    <div class="progress mt-4" style="height: 4px;">
-                        <div class="progress-bar bg-warning" role="progressbar" style="width: 100%" aria-valuenow="68" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <span class="float-right">100%</span>
-                </div>
+            <h3 class="mt-4">43,225</h3>
+            <div class="progress mt-4" style="height: 4px;">
+                <div class="progress-bar bg-primary" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
             </div>
-        </div>
-
-        <div class="col-sm-6 col-xl-3">
-            <div class="card">
-                <div class="card-heading p-4">
-                    <div class="mini-stat-icon float-right">
-                        <i class="mdi mdi-buffer bg-danger text-white"></i>
-                    </div>
-                    <div>
-                        <h5 class="font-20">Categorias nuevas</h5>
-                    </div>
-                    <h3 class="mt-4">1 </h3>
-                    <div class="progress mt-4" style="height: 4px;">
-                        <div class="progress-bar bg-danger" role="progressbar" style="width: 100%" aria-valuenow="82" aria-valuemin="0" aria-valuemax="100"></div>
-                    </div>
-                    <span class="float-right">82%</span>
-                </div>
-            </div>
+            <p class="text-muted mt-2 mb-0">Previous period<span class="float-right">75%</span></p>
         </div>
     </div>
-    <p> </p>
+</div>
+
+<div class="col-sm-6 col-xl-3">
+    <div class="card">
+        <div class="card-heading p-4">
+            <div class="mini-stat-icon float-right">
+                <i class="mdi mdi-briefcase-check bg-success text-white"></i>
+            </div>
+            <div>
+                <h5 class="font-16">Total Revenue</h5>
+            </div>
+            <h3 class="mt-4">$73,265</h3>
+            <div class="progress mt-4" style="height: 4px;">
+                <div class="progress-bar bg-success" role="progressbar" style="width: 88%" aria-valuenow="88" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <p class="text-muted mt-2 mb-0">Previous period<span class="float-right">88%</span></p>
+        </div>
+    </div>
+</div>
+
+<div class="col-sm-6 col-xl-3">
+    <div class="card">
+        <div class="card-heading p-4">
+            <div class="mini-stat-icon float-right">
+                <i class="mdi mdi-tag-text-outline bg-warning text-white"></i>
+            </div>
+            <div>
+                <h5 class="font-16">Average Price</h5>
+            </div>
+            <h3 class="mt-4">447</h3>
+            <div class="progress mt-4" style="height: 4px;">
+                <div class="progress-bar bg-warning" role="progressbar" style="width: 68%" aria-valuenow="68" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <p class="text-muted mt-2 mb-0">Previous period<span class="float-right">68%</span></p>
+        </div>
+    </div>
+</div>
+
+<div class="col-sm-6 col-xl-3">
+    <div class="card">
+        <div class="card-heading p-4">
+            <div class="mini-stat-icon float-right">
+                <i class="mdi mdi-buffer bg-danger text-white"></i>
+            </div>
+            <div>
+                <h5 class="font-16">Add to Card</h5>
+            </div>
+            <h3 class="mt-4">86%</h3>
+            <div class="progress mt-4" style="height: 4px;">
+                <div class="progress-bar bg-danger" role="progressbar" style="width: 82%" aria-valuenow="82" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+            <p class="text-muted mt-2 mb-0">Previous period<span class="float-right">82%</span></p>
+        </div>
+    </div>
+</div>
+
+</div>
+
+<p> </p>
+<!-- end col -->
+</div>
     <div class="row">
                         <div class="col-xl-6">
                             <div class="card m-b-30">
@@ -98,7 +103,7 @@
                                         <a href="#" class="friends-suggestions-list">
                                             <div class="border-bottom position-relative">
                                                 <div class="float-left mb-0 mr-3">
-                                                    <img src="public/Assets/images/Icono.jpg" alt="" class="rounded-circle thumb-md">
+                                                    <img src="Public/Assets/images/upload.png" alt="" width="100px" height="73px" class="rounded-circle">
                                                 </div>
                                                 <div class="suggestion-icon float-right mt-2 pt-1">
                                                     <i class="mdi mdi-plus"></i>
@@ -106,7 +111,7 @@
 
                                                 <div class="desc">
                                                     <h5 class="font-14 mb-1 pt-2 text-dark">Subir Archivo</h5>
-                                                    <p class="text-muted">3 Friend suggest</p>
+                                                    <p class="text-muted">-</p>
                                                 </div>
                                             </div>
                                         </a>
@@ -114,7 +119,7 @@
                                         <a href="#" class="friends-suggestions-list">
                                             <div class="border-bottom position-relative">
                                                 <div class="float-left mb-0 mr-3">
-                                                    <img src="assets/images/users/user-3.jpg" alt="" class="rounded-circle thumb-md">
+                                                    <img src="Public/Assets/images/search.png" alt="" width="100px" height="73px" class="rounded-circle">
                                                 </div>
                                                 <div class="suggestion-icon float-right mt-2 pt-1">
                                                     <i class="mdi mdi-plus"></i>
@@ -122,7 +127,7 @@
 
                                                 <div class="desc">
                                                     <h5 class="font-14 mb-1 pt-2 text-dark">Consultar Archivo</h5>
-                                                    <p class="text-muted">17 Friend suggest</p>
+                                                    <p class="text-muted">-</p>
                                                 </div>
                                             </div>
                                         </a>
@@ -130,7 +135,7 @@
                                         <a href="#" class="friends-suggestions-list">
                                             <div class="border-bottom position-relative">
                                                 <div class="float-left mb-0 mr-3">
-                                                    <img src="assets/images/users/user-4.jpg" alt="" class="rounded-circle thumb-md">
+                                                <img src="Public/Assets/images/categories.png" alt="" width="100px" height="73px" class="rounded-circle">
                                                 </div>
                                                 <div class="suggestion-icon float-right mt-2 pt-1">
                                                     <i class="mdi mdi-plus"></i>
@@ -138,7 +143,7 @@
 
                                                 <div class="desc">
                                                     <h5 class="font-14 mb-1 pt-2 text-dark">Crear Categorias</h5>
-                                                    <p class="text-muted">12 Friend suggest</p>
+                                                    <p class="text-muted">-</p>
                                                 </div>
                                             </div>
                                         </a>
@@ -146,7 +151,7 @@
                                         <a href="#" class="friends-suggestions-list">
                                             <div class="border-bottom position-relative">
                                                 <div class="float-left mb-0 mr-3">
-                                                    <img src="assets/images/users/user-5.jpg" alt="" class="rounded-circle thumb-md">
+                                                <img src="Public/Assets/images/usuario.png" alt="" width="100px" height="73px" class="rounded-circle">
                                                 </div>
                                                 <div class="suggestion-icon float-right mt-2 pt-1">
                                                     <i class="mdi mdi-plus"></i>
@@ -154,7 +159,7 @@
 
                                                 <div class="desc">
                                                     <h5 class="font-14 mb-1 pt-2 text-dark">Registrar Usuario</h5>
-                                                    <p class="text-muted">18 Friend suggest</p>
+                                                    <p class="text-muted">-</p>
                                                 </div>
                                             </div>
                                         </a>
